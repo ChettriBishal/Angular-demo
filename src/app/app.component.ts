96be8c9f3cea7c73bb1eed64f57cc1ae880ed100
Bishal Chettri <bistimulus0@gmail.com>
2022-09-08 16:28:35 +0530

    basic calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  count = 1;
+
+  listOfNumbers = ['Gangtok','Namchi','Pakyong','Yangang','Changu'];
+  
+
+  decreaseCount = ()=>{
+    console.log('Button Click')
+    this.count--;
+  }
+  increaseCount = () =>{
+    this.count++;
+  }
+  arr = [0,1,2,3,4,5,6,7,8,9];
+  A = 0;
+  B = 0;
+  C = 0;
+  add = () =>{
+    this.C = this.A + this.B;
+  }
+  subtract = () =>{
+    this.C = this.A - this.B;
+  }
+  multiply = () =>{
+    this.C = this.A * this.B;
+  }
+  divide = () =>{
+    this.C = this.A / this.B;
+  }
+  
 }
